739c515acaf55f9df28c12be4e2ab627c3cc8927
Dardo Camaño <dardocam@gmail.com>
2025-05-02 22:30:49 -0300

    adding

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Item } from './Item';
+
+@Component({
+  selector: 'app-item-list',
+  standalone: false,
+  templateUrl: './item-list.component.html',
+  styleUrl: './item-list.component.scss'
+})
+export class ItemListComponent {
+
+  items: Item[] = [
+    {
+      id: 1,
+      name: 'Item 1',
+      description: 'Description for Item 1',
+      price: 10.99,
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png',
+      category: 'Category 1',
+      stock: 5
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+      description: 'Description for Item 2',
+      price: 20.99,
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/132.png',
+      category: 'Category 2',
+      stock: 0
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+      description: 'Description for Item 3',
+      price: 30.99,
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png',
+      category: 'Category 3',
+      stock: 3
+    },
+    // Add more items as needed
+  ];
+
+}
